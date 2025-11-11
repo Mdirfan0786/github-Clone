bc40aa7a83d89c64f67d43965659b32e594f3531
Md Irfan <rn555666777@gmail.com>
2025-11-11 15:41:47 +0530

    Reading the terminal command from the users

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const { initRepo } = require("./controllers/init.js");
+const { addRepo } = require("./controllers/add.js");
+const { commitRepo } = require("./controllers/commit.js");
+const { pullRepo } = require("./controllers/pull.js");
+const { pushRepo } = require("./controllers/push.js");
+const { revertRepo } = require("./controllers/revert.js");
+
+yargs(hideBin(process.argv))
+  .command("init", "initialize a new repository", {}, initRepo)
+  .command(
+    "add <file>",
+    "add a file to the repository",
+    (yargs) => {
+      yargs.positional("file", {
+        describe: "File to add to the staging area",
+        type: "string",
+      });
+    },
+    addRepo
+  )
+  .command(
+    "commit <message>",
+    "commit the staged files",
+    (yargs) => {
+      yargs.positional("message", {
+        describe: "commit message",
+        type: "string",
+      });
+    },
+    commitRepo
+  )
+  .command("pull", "pull commits from S3", {}, pullRepo)
+  .command("push", "push commits to S3", {}, pushRepo)
+  .command(
+    "revert <commitID>",
+    "Revert to a specific commit",
+    (yargs) => {
+      yargs.positional("commitID", {
+        describe: "commit ID to revert to",
+        type: "string",
+      });
+    },
+    commitRepo
+  )
+  .demandCommand(1, "You need atleast one command!")
+  .help().argv;
