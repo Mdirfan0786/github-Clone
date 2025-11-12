ce96339166c93cb2a6e996ff67aeabce8a4aecb9
Md Irfan <rn555666777@gmail.com>
2025-11-12 22:55:54 +0530

    Working on the pullChanges Logic

--- backend/controllers/pull.js
+++ backend/controllers/pull.js
@@ -1,5 +1,52 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { s3, S3_BUCKET } = require("../config/aws-config");
+
 async function pullRepo() {
-  console.log("pull command called");
+  const repoPath = path.resolve(process.cwd(), ".apnaGit");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    const data = await s3
+      .listObjectsV2({
+        Bucket: S3_BUCKET,
+        Prefix: "commits/",
+      })
+      .promise();
+
+    const objects = data.Contents || [];
+
+    if (objects.length === 0) {
+      console.log(" No commits found in S3 bucket.");
+      return;
+    }
+
+    for (const object of objects) {
+      if (!object.Key || object.Key.endsWith("/")) continue;
+
+      const key = object.Key;
+      const commitDir = path.join(
+        commitsPath,
+        path.dirname(key).split("/").pop()
+      );
+
+      await fs.mkdir(commitDir, { recursive: true });
+
+      const params = {
+        Bucket: S3_BUCKET,
+        Key: key,
+      };
+
+      const fileContent = await s3.getObject(params).promise();
+      await fs.writeFile(path.join(repoPath, key), fileContent.Body);
+
+      console.log(` Pulled: ${key}`);
+    }
+
+    console.log("All commits pulls from S3");
+  } catch (err) {
+    console.error("unable to pull", err);
+  }
 }
 
 module.exports = { pullRepo };
