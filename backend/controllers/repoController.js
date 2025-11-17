49beff4eceae605e7d60f4562a3b1068a455a4d9
Md Irfan <rn555666777@gmail.com>
2025-11-17 17:18:48 +0530

    fetching repository details

--- backend/controllers/repoController.js
+++ backend/controllers/repoController.js
@@ -29,21 +29,52 @@ async function createRepository(req, res) {
       .status(201)
       .json({ message: "Repository Created!", repositoryID: result._id });
   } catch (err) {
-    console.error("Error while creating Repository : ", err.message);
+    console.error("Error while Repository creation : ", err.message);
     res.status(500).json({ message: "Server Error!" });
   }
 }
 
 async function getAllRepositories(req, res) {
-  res.send("getting all repositories!");
+  try {
+    const allRepos = await Repository.find({})
+      .populate("owner")
+      .populate("issues");
+
+    res.send(allRepos);
+  } catch (err) {
+    console.error("Error while fetching repositories : ", err.message);
+    res.status(500).json({ message: "Server Error!" });
+  }
 }
 
 async function fetchRepositoryById(req, res) {
-  res.send("fetching repository By Id... ");
+  const { id } = req.params;
+
+  try {
+    const repository = await Repository.find({ _id: id })
+      .populate("owner")
+      .populate("issues");
+
+    res.send(repository);
+  } catch (err) {
+    console.error("Error while fetching repository : ", err.message);
+    res.status(500).json({ message: "Server Error!" });
+  }
 }
 
 async function fetchRepositoryByName(req, res) {
-  res.send("fetching repository By name... ");
+  const { name } = req.params;
+
+  try {
+    const repository = await Repository.find({ name })
+      .populate("owner")
+      .populate("issues");
+
+    res.send(repository);
+  } catch (err) {
+    console.error("Error while fetching repository by name : ", err.message);
+    res.status(500).json({ message: "Server Error!" });
+  }
 }
 
 async function fetchRepositoriesForCurrentUser(req, res) {
