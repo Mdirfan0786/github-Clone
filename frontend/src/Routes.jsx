925d66806b88072368c28497fc2bcb1b333de439
Md Irfan <rn555666777@gmail.com>
2025-11-18 15:09:24 +0530

    working on the frontend router

---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from "react";
+import { useNavigate, useRoutes } from "react-router-dom";
+
+// Pages
+
+import Dashboard from "./components/daashboard/Dashboard";
+import Profile from "./components/user/Profile";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+
+// Auth Context
+
+import { useAuth } from "./authContext";
+
+const ProjectRoutes = () => {
+  const [currentUser, setCurrentUser] = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userIdFromStorage = localStorage.getItem("userId");
+
+    if (userIdFromStorage && !currentUser) {
+      setCurrentUser(userIdFromStorage);
+    }
+
+    if (
+      !userIdFromStorage &&
+      !["/login", "signup"].includes(window.location.pathname)
+    ) {
+      navigate("/login");
+    }
+
+    if (userIdFromStorage && window.location.pathname == "/login") {
+      navigate("/");
+    }
+
+    let element = useRoutes([
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ]);
+
+    return element;
+  }, [currentUser, navigate, setCurrentUser]);
+};
+
+export default ProjectRoutes;
