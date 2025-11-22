2f5cfbb7b61c4891661c8e8704dc8ae5f79c9527
Md Irfan <rn555666777@gmail.com>
2025-11-22 22:27:08 +0530

    changes in environment

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD
   ? "https://github-clone-backend-hbpl.onrender.com"
-  : "http://localhost:8000";
+  : "http://localhost:3000";
 
 export default server;
