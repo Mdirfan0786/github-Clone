d049fecb1f1f67ad139f83593c04cee2f5ee2e6d
Md Irfan <rn555666777@gmail.com>
2025-11-27 19:20:21 +0530

    environment.js change

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
-const server =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:3000"
-    : "https://github-clone-backend-hbpl.onrender.com";
+let IS_PROD = true;
+const server = IS_PROD
+  ? "https://github-clone-backend-hbpl.onrender.com"
+  : "http://localhost:3000";
 
 export default server;
